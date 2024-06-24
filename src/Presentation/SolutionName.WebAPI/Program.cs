d6dcf91a91a93d5bebaf40bbc5c743a5e9c5ae7a
javidsevdimaliyev <javid.sevdimaliyev@outlook.com>
2024-06-25 03:01:11 +0400

    error fixes

--- src/Presentation/SolutionName.WebAPI/Program.cs
+++ src/Presentation/SolutionName.WebAPI/Program.cs
@@ -13,23 +13,6 @@ using SolutionName.WebAPI.RegistrationExtensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.SetEnvironment();
-//#region Env operations
-//args = Environment.GetCommandLineArgs();
-//var envArg = Array.IndexOf(args, "--environment");
-//var envFromArgs = envArg >= 0 ? args[envArg + 1] : null;
-//var aspnetcorenv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-//var dotnetcorenv = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
-//var environment = envFromArgs ?? (string.IsNullOrWhiteSpace(aspnetcorenv)
-//    ? dotnetcorenv
-//    : aspnetcorenv);
-//Console.WriteLine($@"Current ENV :{environment}");
-//builder.Configuration.AddCommandLine(Environment.GetCommandLineArgs())
-//       .AddJsonFile("appsettings.json", false, true)
-//       .AddJsonFile($"appsettings.{environment}.json", true);
-
-//#endregion
-
-
 
 /// Add CORS policy
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
